f099bc3e499e4f761af85ba4e07be62bc263f419
odiat <troy.odia@ontariotechu.net>
2024-09-26 19:19:35 -0400

    base set-up to regsiter a user

--- api/contollers/auth.js
+++ api/contollers/auth.js
@@ -1,7 +1,10 @@
-const register = (req, res) => {
-  res.send("regsiter");
+const { StatusCodes } = require("http-status-codes");
+const User = require("../models/User");
+const register = async (req, res) => {
+  const user = await User.create(req.body);
+  res.status(StatusCodes.OK).json({ user });
 };
-const login = (req, res) => {
+const login = async (req, res) => {
   res.send("login buddy");
 };
 module.exports = {
