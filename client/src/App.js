90ff3902247b35fec8a51e59c94ecad9ed767db5
odiat <troy.odia@ontariotechu.net>
2024-09-26 18:07:49 -0400

    initial structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import RegisterScreen from "./Register";
+
+function App() {
+  return (
+    <>
+      <RegisterScreen></RegisterScreen>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Register.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { useState } from "react";
+export default function RegisterScreen() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  useEffect(() => {
+    console.log(username, password);
+  });
+
+  return (
+    <div className="bg-amber-100 h-screen flex justify-center items-center">
+      <form
+        className="m-3 mb-12 flex w-[500px] min-w-[200px] flex-col 
+      space-y-3 text-black text-2xl font-medium"
+      >
+        <input
+          type="text"
+          className="rounded px-4 py-4 w-full"
+          placeholder="username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        ></input>
+        <input
+          type="password"
+          className="rounded px-4 py-4 w-full"
+          placeholder="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+        <button className="rounded bg-amber-700 px-4 py-4 text-white">
+          Login
+        </button>
+        <p className="mx-auto text-black pt-2">
+          Don't have an account? Register
+        </p>
+      </form>
+    </div>
+  );
+}
