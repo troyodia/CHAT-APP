fab76827801536f675a63f7ef3b32db3ef49e9e0
odiat <troy.odia@ontariotechu.net>
2024-09-26 18:37:50 -0400

    create connection to port and mongodb

--- api/app.js
+++ api/app.js
@@ -1,1 +1,24 @@
-console.log("start");
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db/connect");
+const app = express();
+
+app.use(express.json());
+
+app.get("/home", (req, res) => {
+  res.send("home");
+});
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log("listening on port " + port);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
