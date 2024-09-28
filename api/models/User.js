30c5517d0dea391bc3e2e4a9df18af7ea1e6075a
odiat <troy.odia@ontariotechu.net>
2024-09-28 19:35:48 -0400

    added timestamps to user model

--- api/models/User.js
+++ api/models/User.js
@@ -2,34 +2,37 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const UserSchema = mongoose.Schema({
-  firstname: {
-    type: String,
-    required: [true, "please provide user first name"],
-    maxLength: 30,
-    minLength: 2,
+const UserSchema = mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: [true, "please provide user first name"],
+      maxLength: 30,
+      minLength: 2,
+    },
+    lastname: {
+      type: String,
+      required: [true, "please provide user last name"],
+      maxLength: 30,
+      minLength: 2,
+    },
+    email: {
+      type: String,
+      required: [true, "please provide user email"],
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "provide vaild email",
+      ],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "please provide user password"],
+      minLength: 6,
+    },
   },
-  lastname: {
-    type: String,
-    required: [true, "please provide user last name"],
-    maxLength: 30,
-    minLength: 2,
-  },
-  email: {
-    type: String,
-    required: [true, "please provide user email"],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "provide vaild email",
-    ],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "please provide user password"],
-    minLength: 6,
-  },
-});
+  { timestamps: true }
+);
 
 UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
