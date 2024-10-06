35841948920ccabe63d1f1070a29b0b6ff0b2e63
odiat <troy.odia@ontariotechu.net>
2024-10-05 20:10:54 -0400

    added toggle functionality for shared photos on details section

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -3,8 +3,10 @@ import arrowUp from "../../images/icons/uparrow.png";
 import arrowDown from "../../images/icons/downarrow.png";
 import plusIcon from "../../images/icons/plus.png";
 import minusIcon from "../../images/icons/minus.png";
+import { useState } from "react";
 
 export default function Detail() {
+  const [toggle, setToggle] = useState(false);
   return (
     <div className="flex flex-col items-center border border-y-0 border-r-0 border-l-white/30 ml-auto w-[370px]">
       <div className="w-24 h-24 my-4">
@@ -22,100 +24,114 @@ export default function Detail() {
         <ul className="w-full list-none mt-5">
           <li className="flex w-full mb-8 font-semibold">
             <div>Chat Settings</div>
-            <div className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
+            <button className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
               <img className="w-3" src={arrowUp} alt=""></img>
-            </div>
+            </button>
           </li>
           <li className="flex w-full mb-8 font-semibold">
             <div>Privacy & Help</div>
-            <div className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
+            <button className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
               <img className="w-3" src={arrowUp} alt=""></img>
-            </div>
+            </button>
           </li>
           <li className="flex w-full font-semibold mb-4">
             <div>Shared Photos</div>
-            <div className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
-              <img className="w-3" src={arrowUp} alt=""></img>
-            </div>
+            <button
+              className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center"
+              onClick={() => {
+                setToggle((prev) => !prev);
+              }}
+            >
+              <img
+                className="w-3"
+                src={toggle ? arrowDown : arrowUp}
+                alt=""
+              ></img>
+            </button>
           </li>
-          <li className="flex w-full mb-8 h-52 overflow-auto">
-            <ul className="w-full list-none">
-              <li className="flex w-full items-center font-semibold mb-4">
-                <div className="w-10 h-10 mr-5">
-                  <img
-                    className="w-10 h-10 rounded-md object-cover"
-                    src={defaultImg}
-                    alt=""
-                  ></img>
-                </div>
-                <div className="text-xs ">Image name from computer input</div>
-                <div className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
-                  <img className="w-3" src={plusIcon} alt=""></img>
-                </div>
-              </li>
-              <li className="flex w-full items-center font-semibold mb-4">
-                <div className="w-10 h-10 mr-5">
-                  <img
-                    className="w-10 h-10 rounded-md object-cover"
-                    src={defaultImg}
-                    alt=""
-                  ></img>
-                </div>
-                <div className="text-xs ">Image name from computer input</div>
-                <div className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
-                  <img className="w-3" src={plusIcon} alt=""></img>
-                </div>
-              </li>
-              <li className="flex w-full items-center font-semibold mb-4">
-                <div className="w-10 h-10 mr-5">
-                  <img
-                    className="w-10 h-10 rounded-md object-cover"
-                    src={defaultImg}
-                    alt=""
-                  ></img>
-                </div>
-                <div className="text-xs ">Image name from computer input</div>
-                <div className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
-                  <img className="w-3" src={plusIcon} alt=""></img>
-                </div>
-              </li>
-              <li className="flex w-full items-center font-semibold mb-4">
-                <div className="w-10 h-10 mr-5">
-                  <img
-                    className="w-10 h-10 rounded-md object-cover"
-                    src={defaultImg}
-                    alt=""
-                  ></img>
-                </div>
-                <div className="text-xs ">Image name from computer input</div>
-                <div className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
-                  <img className="w-3" src={plusIcon} alt=""></img>
-                </div>
-              </li>
-              <li className="flex w-full items-center font-semibold mb-4">
-                <div className="w-10 h-10 mr-5">
-                  <img
-                    className="w-10 h-10 rounded-md object-cover"
-                    src={defaultImg}
-                    alt=""
-                  ></img>
-                </div>
-                <div className="text-xs ">Image name from computer input</div>
-                <div className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
-                  <img className="w-3" src={plusIcon} alt=""></img>
-                </div>
-              </li>
-            </ul>
-          </li>
+          {toggle ? (
+            <li className="flex w-full mb-8 h-52 overflow-auto">
+              <ul className="w-full list-none ">
+                <li className="flex w-full items-center font-semibold mb-4">
+                  <div className="w-10 h-10 mr-5">
+                    <img
+                      className="w-10 h-10 rounded-md object-cover"
+                      src={defaultImg}
+                      alt=""
+                    ></img>
+                  </div>
+                  <div className="text-xs ">Image name from computer input</div>
+                  <button className="w-6 h-6 rounded-full ml-auto mr-2 bg-white/10 flex items-center justify-center">
+                    <img className="w-3" src={plusIcon} alt=""></img>
+                  </button>
+                </li>
+                <li className="flex w-full items-center font-semibold mb-4">
+                  <div className="w-10 h-10 mr-5">
+                    <img
+                      className="w-10 h-10 rounded-md object-cover"
+                      src={defaultImg}
+                      alt=""
+                    ></img>
+                  </div>
+                  <div className="text-xs ">Image name from computer input</div>
+                  <button className="w-6 h-6 rounded-full ml-auto mr-2 bg-white/10 flex items-center justify-center">
+                    <img className="w-3" src={plusIcon} alt=""></img>
+                  </button>
+                </li>
+                <li className="flex w-full items-center font-semibold mb-4">
+                  <div className="w-10 h-10 mr-5">
+                    <img
+                      className="w-10 h-10 rounded-md object-cover"
+                      src={defaultImg}
+                      alt=""
+                    ></img>
+                  </div>
+                  <div className="text-xs ">Image name from computer input</div>
+                  <button className="w-6 h-6 rounded-full ml-auto mr-2 bg-white/10 flex items-center justify-center">
+                    <img className="w-3" src={plusIcon} alt=""></img>
+                  </button>
+                </li>
+                <li className="flex w-full items-center font-semibold mb-4">
+                  <div className="w-10 h-10 mr-5">
+                    <img
+                      className="w-10 h-10 rounded-md object-cover"
+                      src={defaultImg}
+                      alt=""
+                    ></img>
+                  </div>
+                  <div className="text-xs ">Image name from computer input</div>
+                  <button className="w-6 h-6 rounded-full ml-auto mr-2 bg-white/10 flex items-center justify-center">
+                    <img className="w-3" src={plusIcon} alt=""></img>
+                  </button>
+                </li>
+                <li className="flex w-full items-center font-semibold mb-4">
+                  <div className="w-10 h-10 mr-5">
+                    <img
+                      className="w-10 h-10 rounded-md object-cover"
+                      src={defaultImg}
+                      alt=""
+                    ></img>
+                  </div>
+                  <div className="text-xs ">Image name from computer input</div>
+                  <button className="w-6 h-6 rounded-full ml-auto mr-2 bg-white/10 flex items-center justify-center">
+                    <img className="w-3" src={plusIcon} alt=""></img>
+                  </button>
+                </li>
+              </ul>
+            </li>
+          ) : (
+            <li className="mt-8"></li>
+          )}
+
           <li className="flex w-full mb-8 font-semibold">
             <div>Shared Files</div>
-            <div className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
+            <button className="w-6 h-6 rounded-full ml-auto bg-white/10 flex items-center justify-center">
               <img className="w-3" src={arrowUp} alt=""></img>
-            </div>
+            </button>
           </li>
         </ul>
       </div>
-      <div className="flex flex-col w-full mt-auto px-6 font-bold text-lg mb-10">
+      <div className="flex flex-col w-full mt-auto px-6 font-bold text-lg mb-7">
         <button className="text-center py-4 bg-red-800 rounded-md mb-6">
           Block User
         </button>
